54d5b835e3e10f57704b21aab77e90165afd4871
Scott Duffy <duffyscottc@gmail.com>
2016-08-12 14:58:26 -0400

    Build an app that lets you add and remove items from the list.

--- MyToDoList/FirstViewController.swift
+++ MyToDoList/FirstViewController.swift
@@ -8,12 +8,68 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet var table: UITableView!
+    
+    var items: [String] = [] //Defined as a global variable (empty array) because we will need to access it
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return items.count //The number of items in the array
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        
+        cell.textLabel?.text = items[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        let itemsObject = UserDefaults.standard.object(forKey: "items") //Placeholder for the information in UserDefaults
+        
+        if let tempItems = itemsObject as? [String] { //Is there anything listed under that key?
+            
+            //If that worked just above, we can let:
+            items = tempItems
+            
+        }
+        
+        table.reloadData()
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            
+            items.remove(at: indexPath.row)
+            
+            table.reloadData()
+            
+            UserDefaults.standard.set(items, forKey: "items")
+            
+        }
+        
+        
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Test")
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
--- MyToDoList/SecondViewController.swift
+++ MyToDoList/SecondViewController.swift
@@ -8,8 +8,57 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet var itemTextField: UITextField!
+    
+    @IBAction func add(_ sender: AnyObject) {
+        
+        //First we need to initially set "items" as an array of strings
+        var items: [String]
+        
+        //So we're either adding on the contents of the textfield to the existing array, or we are creating a new array
+        
+        //1
+        let itemsObject = UserDefaults.standard.object(forKey: "items") //Second let's attempt to pull the array from memory
+        //2
+        if let tempItems = itemsObject as? [String]/*an array of strings*/ { //Third We can use this to check if pulling the array was successful.
+            
+            //If that worked just above, we can let
+            items = tempItems
+            
+            //3
+            items.append(itemTextField.text!) //add an item 
+            
+            
+        } else { //4
+            //If that was not successful above
+            
+            items = [itemTextField.text!]
+            
+        }
+        
+        //Now we need to save "items"
+        UserDefaults.standard.set(items, forKey: "items")
+        
+        //Now that we've saved these items, we need to do something similar in the FirstViewController. See viewDidLoad
+        
+        itemTextField.text = "" //Allow the user to do this again easily
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        self.view.endEditing(true)
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
